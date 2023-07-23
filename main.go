c84ee91c3ab172d929a4689338b8bec7f408899a
Adiatma Kamarudin <adiatma.mail@gmail.com>
2023-07-23 13:19:00 +0800

    feat: add image variations functionality
    Add functionality to generate image variations using the provided image file.
    
    The main.go file was modified to include the following changes:
    - Import the os package to use the environment variable for the OpenAI API Key.
    - Modify the configuration to use the OpenAI API Key from the environment variable.
    - Replace the previous chat completions code with the new images variations code.
    - Use the ./assets/example-img.png file as the input image for generating variations.
    - Print the response from the API.
    
    The new functionality allows generating different variations of an image.
    
    Note: The commented code for completions and JSON serialization was removed for simplicity.

--- main.go
+++ main.go
@@ -2,36 +2,19 @@ package main
 
 import (
 	"fmt"
+	"os"
 
 	gochatgptsdk "github.com/ak9024/go-chatgpt-sdk"
 )
 
 func main() {
 	c := gochatgptsdk.NewConfig(gochatgptsdk.Config{
-		OpenAIKey: "",
+		OpenAIKey: os.Getenv("OPENAI_API_KEY"),
 	})
 
-	resp, _ := c.ChatCompletions(gochatgptsdk.ModelChat{
-		Model: "gpt-3.5-turbo-16k",
-		Messages: []gochatgptsdk.Message{
-			{
-				Role:    "system",
-				Content: "You are a devops engineer",
-			},
-			{
-				Role:    "user",
-				Content: "Please help me to create Dockerfile for golang with a multistage image",
-			},
-		},
+	resp, _ := c.ImagesVariations(gochatgptsdk.ModelImagesVariations{
+		Image: "./assets/example-img.png",
 	})
 
-	// resp, _ := c.Completions(gochatgptsdk.ModelText{
-	// 	Model:  "text-davinci-003",
-	// 	Prompt: "What the weather today?",
-	// })
-
-	fmt.Println(resp.Choices[len(resp.Choices)-1].Message)
-
-	//m, _ := json.Marshal(resp)
-	//fmt.Println(string(m))
+	fmt.Println(resp)
 }
